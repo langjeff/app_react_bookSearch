797886732633aa7b9eb2c2f07b5d467fa5752b8b
langjeff <lang.jeffrey@gmail.com>
2021-03-09 18:15:47 -0700

    heroku build

---
+++ client/build/precache-manifest.29b0e06677ff8ac8ec110b7ecb4225ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987dcf4874e949c4891ba1c9f045eccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a42d4c2c3acdf766fecb",
+    "url": "/static/css/2.82f4764f.chunk.css"
+  },
+  {
+    "revision": "a42d4c2c3acdf766fecb",
+    "url": "/static/js/2.cbbcccfc.chunk.js"
+  },
+  {
+    "revision": "51e2e15551f96ce90c04c08534577337",
+    "url": "/static/js/2.cbbcccfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543cb4fe9ba24c00f0cc",
+    "url": "/static/js/main.eca9f914.chunk.js"
+  },
+  {
+    "revision": "43bc4bf1fd58020a4de5",
+    "url": "/static/js/runtime-main.54f3f186.js"
+  }
+]);
